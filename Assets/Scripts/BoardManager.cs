49fcededdfb05118f5bb122cc4c116d5e9d59fa5
Ash <afalzon12@gmail.com>
2024-02-28 13:45:03 +0100

    board + spawning + pieces

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KawaiiDesu
+{
+    public class BoardManager : MonoBehaviour
+    {
+        public PiecesSO[,] board = new PiecesSO[3, 4];
+        public bool[,] isOccupied = new bool[3, 4];
+
+        public Transform[,] cells = new Transform[3, 4];
+
+        [SerializeField] private PieceBehavior piecePrefab;
+
+        [Header("All lists to initialize the board")]
+        [SerializeField] private List<Transform> cellList = new List<Transform>();
+        [SerializeField] private List<PiecesSO> piecesInitialPositionList = new List<PiecesSO>();
+
+        void Start()
+        {
+            InitializeAllGrids();
+        }
+
+        private void InitializeAllGrids()
+        {
+            int index = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    cells[i,j] = cellList[index];
+                    board[i,j] = piecesInitialPositionList[index];
+
+                    if (board[i, j] != null)
+                    {
+                        PieceBehavior newPiece = Instantiate(piecePrefab, cells[i, j]);
+                        newPiece.PieceData = board[i, j];
+                        newPiece.Init();
+                    }
+
+                    index++;
+                }
+            }
+        }
+    }
+}
+
