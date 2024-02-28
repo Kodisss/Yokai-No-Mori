4fa83cfe818b9c42ef02769b389fe30145bc7edc
Ash <afalzon12@gmail.com>
2024-02-28 13:59:33 +0100

    Update PieceBehavior.cs

--- Assets/Scripts/PieceBehavior.cs
+++ Assets/Scripts/PieceBehavior.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace KawaiiDesu
 {
     public class PieceBehavior : MonoBehaviour
@@ -10,8 +9,10 @@ namespace KawaiiDesu
         private PiecesSO _pieceData;
         private SpriteRenderer _pieceSprite;
         private bool _side = true;
+
         public bool Side { get => _side; set => _side = value; }
         public PiecesSO PieceData { get => _pieceData; set => _pieceData = value; }
+
         public void Init()
         {
             _pieceSprite = GetComponent<SpriteRenderer>();
