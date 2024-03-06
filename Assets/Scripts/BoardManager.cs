912edf5be8e268b9bedf8d17a5533ab6f992f1c3
Ash <afalzon12@gmail.com>
2024-03-06 16:58:02 +0100

    it's the same but i spent 1 hour fixing stuff

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,47 +6,67 @@ using UnityEngine;
 
 namespace KawaiiDesu
 {
+    [Serializable]
+    public struct Cell
+    {
+        public Cell(Transform t, bool b)
+        {
+            transform = t;
+            isOccupied = b;
+        }
+
+        public Transform transform;
+        public bool isOccupied;
+    }
+
     public class BoardManager : MonoBehaviour
     {
-        public PiecesSO[,] board = new PiecesSO[3, 4];
-        public bool[,] isOccupied = new bool[3, 4];
-
-        public Transform[,] cells = new Transform[3, 4];
-
         [SerializeField] private PieceBehavior piecePrefab;
         [SerializeField] private Transform cellsParent;
         [SerializeField] private float snapRange;
 
         [Header("All lists to initialize the board")]
-        [SerializeField] private List<Transform> cellList = new List<Transform>();
+        [SerializeField] private List<Cell> cellList = new List<Cell>();
         [SerializeField] private List<PiecesSO> piecesPositionList = new List<PiecesSO>();
 
         void Start()
         {
+            InitializeListOfCells();
             InitializeAllGrids();
         }
 
         private void InitializeAllGrids()
         {
             int index = 0;
-            for (int i = 0; i < 3; i++)
+            int side = 0;
+
+            foreach (PiecesSO piece in piecesPositionList)
             {
-                for(int j = 0; j < 4; j++)
+                if(piece != null)
                 {
-                    cells[i,j] = cellList[index];
-                    board[i,j] = piecesPositionList[index];
-
-                    if (board[i, j] != null)
-                    {
-                        PieceBehavior newPiece = Instantiate(piecePrefab, cells[i, j]);
-                        newPiece.PieceData = board[i, j];
-                        if (j > 1) newPiece.Side = false; //turn the piece if you are opponent
-                        newPiece.Init();
-                        newPiece.dragEndDelegate = SnapObject;
-                    }
-
-                    index++;
+                    PieceBehavior newPiece = Instantiate(piecePrefab, cellList[index].transform);
+                    newPiece.PieceData = piece;
+                    if (side > 1) newPiece.Side = false; //turn the piece if you are opponent
+                    newPiece.Init();
+                    newPiece.dragEndDelegate = SnapObject;
                 }
+                index++;
+                side++;
+                if(side > 3) side = 0;
+            }
+        }
+
+        private void InitializeListOfCells()
+        {
+            List<Transform> child = cellsParent.GetComponentsInChildren<Transform>().ToList<Transform>();
+
+            child.RemoveAll(x => x.tag != "Cell");
+
+            foreach (Transform cell in child)
+            {
+                Cell tempCell = new Cell();
+                tempCell.transform = cell;
+                cellList.Add(tempCell);
             }
         }
 
@@ -61,22 +81,29 @@ namespace KawaiiDesu
                 if (child[i].childCount != 0)
                 {
                     piecesPositionList[i] = child[i].GetComponentInChildren<PieceBehavior>().PieceData;
+                    cellList[i] = new Cell(child[i], true);
                 }
                 else
                 {
                     piecesPositionList[i] = null;
+                    cellList[i] = new Cell(child[i], false);
                 }
             }
         }
 
         public void SnapObject(Transform obj)
         {
-            foreach (Transform point in cellList)
+            foreach (Cell point in cellList)
             {
-                if (Vector2.Distance(point.position, obj.position) <= snapRange)
+                if (Vector2.Distance(point.transform.position, obj.position) <= snapRange)
                 {
+                    if (point.isOccupied/*&& allied*/)
+                    {
+                        obj.localPosition = Vector2.zero;
+                        return;
+                    }
                     obj.parent = null;
-                    obj.parent = point;
+                    obj.parent = point.transform;
                     obj.localPosition = Vector2.zero;
                     CheckPiecesPositions();
                     return;
